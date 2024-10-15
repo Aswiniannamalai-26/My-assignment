0e8e7bf7a42e50575a58a2ad9cd37e9154aa1e67
Aswiniannamalai-26 <akaswini08@gmail.com>
2024-10-16 00:10:42 +0530

    Add files via upload
    Week1day1  Updated-12/10/2024

---
+++ week1day1/Firefox.java
@@ -0,0 +1,21 @@
+package week1day1;
+
+public class Firefox {
+
+	
+        public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float browserVersion = 100.2f;
+		System.out.println("Browser Version is " + browserVersion);
+        String browserName = "Firefox";
+        System.out.println("Browser Name is " + browserName);
+        boolean isVisible = false;
+        System.out.println("It's Visible is " + isVisible);
+        int releaseYear = 1998;
+        System.out.println("ReleaseYear is " + releaseYear);
+        char browserLogo = 'f';
+        System.out.println("BrowserLogo is " + browserLogo);
+        
+	}
+}
